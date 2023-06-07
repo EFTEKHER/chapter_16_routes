34f18bdac5e94d8b95a1fca52adad3d8c719e286
EFTEKHER <eftekherali2000@gmail.com>
2023-06-07 23:25:23 +0600

    done

--- src/Footer.js
+++ src/Footer.js
@@ -1,8 +1,15 @@
+import { useStoreState } from 'easy-peasy';
+
 const Footer = () => {
+  const postCount = useStoreState((state) => state.postCount);
   const today = new Date();
   return (
       <footer className='Footer'>
+      <p>{postCount} Blog Posts</p>
+      <br />
           <p>Copyright &copy; {today.getFullYear()}</p>
+  
+         
       </footer>
   )
 }
